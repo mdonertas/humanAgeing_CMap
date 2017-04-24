b5e109cbda18fc13da05b0124c096e00f0803992
mdonertas <melike@ebi.ac.uk>
2017-04-24 15:16:11 +0100

    microarray vs GTEx

--- scripts/microarray_vs_GTEx.R
+++ scripts/microarray_vs_GTEx.R
@@ -1,22 +1,28 @@
 gtex_cormat=readRDS(file='./data/processed/GTEx/cormat.rds')
 brainmicro_cormat=readRDS(file='./data/processed/humanBrainMicroarray/cormat.rds')
-genes=intersect(rownames(gtex_cormat),rownames(brainmicro_cormat))
-cormat=cbind(gtex_cormat[genes,],brainmicro_cormat[genes,])
+genes=union(rownames(gtex_cormat),rownames(brainmicro_cormat))
+cormat=matrix(NA,ncol=ncol(gtex_cormat)+ncol(brainmicro_cormat),nrow=length(genes),dimnames=list(genes,c(paste('GTEx',colnames(gtex_cormat),sep='_'),sapply(strsplit(colnames(brainmicro_cormat),'_'),function(x)paste(x[1],'_Brain-',x[2],sep='')))))
+cormat[rownames(gtex_cormat),paste('GTEx',colnames(gtex_cormat),sep='_')]=gtex_cormat
+cormat[rownames(brainmicro_cormat),sapply(strsplit(colnames(brainmicro_cormat),'_'),function(x)paste(x[1],'_Brain-',x[2],sep=''))]=brainmicro_cormat
+
 rmat=cormat
 
+dset=sapply(strsplit(colnames(cormat),'_'),function(x)x[1])
+majtis=sapply(strsplit(sapply(strsplit(colnames(cormat),'_'),function(x)x[2]),'-'),function(x)x[1])
 
-
+library(scales)
+library(pheatmap)
 library(ggplot2)
 library(RColorBrewer)
 pc <- prcomp(t(rmat[complete.cases(rmat),]), scale = T)
 
-pcx=cbind(pc$x,data.frame(majtissue=c(sapply(strsplit(rownames(pc$x),'-'),function(x)x[1])[1:35],rep('Brain',ncol(brainmicro_cormat)))))
+pcx=cbind(pc$x,data.frame(majtissue=majtis))
 foo=pcx
 pdf('./results/all_PCA.pdf',width = 10,height = 9)
 ggplot(pcx,aes(x=PC1,y=PC2,color=majtissue))+
   geom_point(size=5,alpha=0.8)+
   theme_bw()+
-  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Pastel2'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
+  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
   xlab(paste("PC 1 (%", round(summary(pc)$importance[2, 1] * 100, 2), ")", sep = ""))+
   ylab(paste("PC 2 (%", round(summary(pc)$importance[2, 2] * 100, 2), ")", sep = ""))+
   theme(axis.title = element_text(size = 15),
@@ -27,7 +33,7 @@ ggplot(pcx,aes(x=PC1,y=PC2,color=majtissue))+
 ggplot(pcx,aes(x=PC3,y=PC4,color=majtissue))+
   geom_point(size=5,alpha=0.8)+
   theme_bw()+
-  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Pastel2'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
+  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
   xlab(paste("PC 1 (%", round(summary(pc)$importance[2, 3] * 100, 2), ")", sep = ""))+
   ylab(paste("PC 2 (%", round(summary(pc)$importance[2, 4] * 100, 2), ")", sep = ""))+
   theme(axis.title = element_text(size = 15),
@@ -41,13 +47,13 @@ dev.off()
 rmat2=rmat[complete.cases(rmat),]
 rmat2=rmat2[!rowMeans(rmat2>0)==1,]>0
 pc <- prcomp(t(rmat2), scale = T)
-pcx=cbind(pc$x,data.frame(majtissue=c(sapply(strsplit(rownames(pc$x),'-'),function(x)x[1])[1:35],rep('Brain',ncol(brainmicro_cormat)))))
+pcx=cbind(pc$x,data.frame(majtissue=majtis))
 foo=pcx
 pdf('./results/all_PCA_type_of_change.pdf',width = 10,height = 9)
 ggplot(pcx,aes(x=PC1,y=PC2,color=majtissue))+
   geom_point(size=5,alpha=0.8)+
   theme_bw()+
-  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Pastel2'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
+  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
   xlab(paste("PC 1 (%", round(summary(pc)$importance[2, 1] * 100, 2), ")", sep = ""))+
   ylab(paste("PC 2 (%", round(summary(pc)$importance[2, 2] * 100, 2), ")", sep = ""))+
   theme(axis.title = element_text(size = 15),
@@ -58,7 +64,7 @@ ggplot(pcx,aes(x=PC1,y=PC2,color=majtissue))+
 ggplot(pcx,aes(x=PC3,y=PC4,color=majtissue))+
   geom_point(size=5,alpha=0.8)+
   theme_bw()+
-  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Pastel2'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
+  scale_color_manual(values = c(brewer.pal(9,'Set1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))])+
   xlab(paste("PC 1 (%", round(summary(pc)$importance[2, 3] * 100, 2), ")", sep = ""))+
   ylab(paste("PC 2 (%", round(summary(pc)$importance[2, 4] * 100, 2), ")", sep = ""))+
   theme(axis.title = element_text(size = 15),
@@ -71,11 +77,13 @@ dev.off()
 
 co=cor(rmat,use = 'pairwise',method = 's')
 pdf('./results/all_correlations.pdf',width=20,height=15)
-annotcol=data.frame(TissueType=c(sapply(strsplit(rownames(pc$x),'-'),function(x)x[1])[1:35],rep('Brain',ncol(brainmicro_cormat))))
+annotcol=data.frame(TissueType=majtis,Dataset=dset)
 rownames(annotcol) <- colnames(co)
-TissueType=c(brewer.pal(9,'Set1'),brewer.pal(8,'Set3'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(annotcol$TissueType))]
+TissueType=c(brewer.pal(9,'Set1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(pcx$majtissue))]
 names(TissueType) <-unique(annotcol$TissueType)
-anno_colors <- list(TissueType = TissueType)
+Dataset=brewer.pal(8,'Pastel2')
+names(Dataset) <-unique(annotcol$Dataset)
+anno_colors <- list(TissueType = TissueType,Dataset=Dataset)
 pheatmap(co,
          scale = 'none',
          annotation_col = annotcol,
@@ -91,143 +99,131 @@ pheatmap(co,
          number_format = '%.1f',number_color = 'black')
 dev.off()
 
-################# 
-# CMap Analysis #
-#################
+###########################
 
-cormat=readRDS(file='./data/processed/GTEx/cormat.rds')
-pmat=readRDS(file='./data/processed/GTEx/pmat.rds')
-padjmat=readRDS(file='./data/processed/GTEx/padjmat.rds')
-exps=readRDS(file='./data/processed/GTEx/exps.rds')
+rm(list=ls())
 
-library(biomaRt)
-humanmart=useMart('ensembl','hsapiens_gene_ensembl')
-getBM(attributes = c('ensembl_gene_id','hgnc_symbol','description'),filters = 'ensembl_gene_id',values = names(which(rowMeans(cormat>0)==1|rowMeans(cormat>0)==0)),mart = humanmart)
+br_up=readRDS('./data/processed/humanBrainMicroarray/all_up_ensembl.rds')
+br_down=readRDS('./data/processed/humanBrainMicroarray/all_down_ensembl.rds')
+gtex_up=readRDS('./data/processed/GTEx/all_up.rds')
+gtex_down=readRDS('./data/processed/GTEx/all_down.rds')
 
-cormat2=cormat
-cormat=cormat[complete.cases(cormat),]
-dim(cormat)
-# [1] 19064    35
+genelist=list(`Brain Microarray\nUp`=br_up,`Brain Microarray\nDown`=br_down,`GTEx Up`=gtex_up,`GTEx Down`=gtex_down)
+library(VennDiagram)
+library(RColorBrewer)
+venn.diagram(genelist,
+             filename = './results/numberofgenes.png',
+             imagetype = 'png',fill=(brewer.pal(4,'Set1')),cat.pos=c(-8,+8,0,0))
 
-quantile(rowSums(cormat>0),probs=c(0.005,1-0.01/2))
+gtex_cormat=readRDS(file='./data/processed/GTEx/cormat.rds')
+brainmicro_cormat=readRDS(file='./data/processed/humanBrainMicroarray/cormat.rds')
+genes=union(rownames(gtex_cormat),rownames(brainmicro_cormat))
+cormat=matrix(NA,ncol=ncol(gtex_cormat)+ncol(brainmicro_cormat),nrow=length(genes),dimnames=list(genes,c(paste('GTEx',colnames(gtex_cormat),sep='_'),sapply(strsplit(colnames(brainmicro_cormat),'_'),function(x)paste(x[1],'_Brain-',x[2],sep='')))))
+cormat[rownames(gtex_cormat),paste('GTEx',colnames(gtex_cormat),sep='_')]=gtex_cormat
+cormat[rownames(brainmicro_cormat),sapply(strsplit(colnames(brainmicro_cormat),'_'),function(x)paste(x[1],'_Brain-',x[2],sep=''))]=brainmicro_cormat
 
-xx=data.frame(`Datasets`=rowSums(cormat>0))
-xx$cons=c('-','Consistent')[1+(xx$Datasets>=32 | xx$Datasets <=4)]
-pdf('./results/GTEx/consistency_distribution.pdf',width = 9)
-ggplot(xx,aes(x=Datasets,fill=cons))+
-  geom_bar(stat='count') +
-  theme_bw()+
-  theme(axis.title = element_text(size = 15),
-        axis.text = element_text(size=12),
-        plot.title = element_text(size=14)) +
-  scale_fill_manual(values=c('gray35','darkred'))+
-  xlab('Number of Datasets')+
-  ylab('Count')+
-  guides(fill=guide_legend(title='')) + 
-  ggtitle('For each gene, how many datasets have a positive correlation with age?\n*Irrespective of the effect size')
-dev.off()
+rmat=cormat
 
-ups=names(which(rowSums(cormat>0)>=32))
-downs=names(which(rowSums(cormat>0)<=4))
+dset=sapply(strsplit(colnames(cormat),'_'),function(x)x[1])
+majtis=sapply(strsplit(sapply(strsplit(colnames(cormat),'_'),function(x)x[2]),'-'),function(x)x[1])
 
-ups2=as.character(read.table('./data/processed/humanBrainMicroarray_LuIncluded/ensembl_consistent_up.txt')[,1])
-downs2=as.character(read.table('./data/processed/humanBrainMicroarray_LuIncluded/ensembl_consistent_down.txt')[,1])
-
-xx=list(ups,downs,ups2,downs2)
-names(xx)=c('GTEx-up','GTEx-down','Brain_micro-up','Brain_micro-down')
-round(sapply(xx,function(x){
-  sapply(xx,function(y){
-    mean(x%in%y)
-  })
-}),3)*100
-
-library('pheatmap')
-
-Var1        <- c("navy", "darkgreen")
-names(Var1) <- c("Exp1", "Exp2")
-anno_colors <- list(Var1 = Var1)
-
-annotcol=data.frame(TissueType=sapply(strsplit(colnames(cormat2),'-'),function(x)x[1]))
-rownames(annotcol) <- colnames(cormat2)
-TissueType=c(brewer.pal(8,'Pastel2'),brewer.pal(8,'Set2'),brewer.pal(8,'Accent'))[1:length(unique(annotcol$TissueType))]
+xx=sapply(genelist,function(x)cormat[x,])
+x=Reduce(rbind,xx)
+annotcol=data.frame(TissueType=majtis,Dataset=dset)
+rownames(annotcol) <- colnames(x)
+TissueType=c(brewer.pal(9,'Set1'),brewer.pal(8,'Dark2'),brewer.pal(8,'Accent'))[1:length(unique(majtis))]
 names(TissueType) <-unique(annotcol$TissueType)
-anno_colors <- list(TissueType = TissueType)
-pdf("./results/GTEx/Brain_microarrayUpsandDowns_inGTEx.pdf")
-pheatmap(cormat2[c(ups2,downs2),],
-         scale = 'none',
-         cluster_rows = F,
-         annotation_col = annotcol,
-         show_rownames = F,
+Dataset=brewer.pal(8,'Pastel2')
+names(Dataset) <-unique(annotcol$Dataset)
+rownames(x)=1:nrow(x)
+annotrow=data.frame(GeneSet=rep(gsub('\n',' ',names(xx)),sapply(xx,nrow)))
+rownames(annotrow)=rownames(x)
+GeneSet=muted(brewer.pal(4,'Set1'))
+names(GeneSet)=unique(annotrow$GeneSet)
+anno_colors <- list(TissueType = TissueType,Dataset=Dataset,GeneSet=GeneSet)
+pdf('./results/microarray_vs_gtex_genesets-nobrain-_expression_heatmap.pdf',width=11,height=15)
+pheatmap(x,
+         annotation_col = annotcol,annotation_row = annotrow,
          annotation_colors = anno_colors,
-         color = colorRampPalette(c(muted('lightblue'),'white',muted('pink')))(50),annotation_names_col = F,gaps_row = length(ups2))
+         color = colorRampPalette(c(muted('lightblue'),'white',muted('pink')))(30),
+         annotation_names_col = F,
+         annotation_names_row = F,
+         show_colnames = T,
+         show_rownames = F, 
+         breaks=seq(-1,1,length.out = 31),cluster_rows = F,gaps_row = cumsum(unname(sapply(xx,nrow)))[-4])
 dev.off()
 
-upsp=unique(getBM(attributes = c('affy_hg_u133a'),filters = 'ensembl_gene_id',values = ups,mart = humanmart)[,1])
-downsp=unique(getBM(attributes = c('affy_hg_u133a'),filters = 'ensembl_gene_id',values = downs,mart = humanmart)[,1])
+####################
+rm(list=ls())
 
-upsp2=setdiff(upsp,downsp)
-downsp2=setdiff(downsp,upsp)
+source('../shared/functions/functions.R')
+br_up=readRDS('./data/processed/humanBrainMicroarray/all_up_ensembl.rds')
+br_down=readRDS('./data/processed/humanBrainMicroarray/all_down_ensembl.rds')
+br_bg=readRDS('./data/processed/humanBrainMicroarray/all_bg_ensembl.rds')
+gtex_up=readRDS('./data/processed/GTEx/noBrain_up.rds')
+gtex_down=readRDS('./data/processed/GTEx/noBrain_down.rds')
+gtex_bg=readRDS('./data/processed/GTEx/noBrain_bg_ensembl.rds')
 
-write.table(upsp2,file = './data/processed/GTEx/all_up.grp',quote = F,row.names = F,col.names = F)
-write.table(downsp2,file = './data/processed/GTEx/all_down.grp',quote = F,row.names = F,col.names = F)
+genelist=setNames(rep(0,length(br_bg)),br_bg)
+genelist[br_up]=1
+br_up_GO=lapply(c('BP','MF','CC'),function(ontx)go_enrich.test(genelist=genelist,selection = function(x)x==1,ontologyx = ontx,nodesize = 10,corrmethod = 'fdr'))
+genelist=setNames(rep(0,length(br_bg)),br_bg)
+genelist[br_down]=1
+br_down_GO=lapply(c('BP','MF','CC'),function(ontx)go_enrich.test(genelist=genelist,selection = function(x)x==1,ontologyx = ontx,nodesize = 10,corrmethod = 'fdr'))
 
-#############################  
-# do the same without brain #
-#############################
+genelist=setNames(rep(0,length(gtex_bg)),gtex_bg)
+genelist[gtex_up]=1
+gtex_up_GO=lapply(c('BP','MF','CC'),function(ontx)go_enrich.test(genelist=genelist,selection = function(x)x==1,ontologyx = ontx,nodesize = 10,corrmethod = 'fdr'))
+genelist=setNames(rep(0,length(gtex_bg)),gtex_bg)
+genelist[gtex_down]=1
+gtex_down_GO=lapply(c('BP','MF','CC'),function(ontx)go_enrich.test(genelist=genelist,selection = function(x)x==1,ontologyx = ontx,nodesize = 10,corrmethod = 'fdr'))
 
-cormat=readRDS(file='./data/processed/GTEx/cormat.rds')
-pmat=readRDS(file='./data/processed/GTEx/pmat.rds')
-padjmat=readRDS(file='./data/processed/GTEx/padjmat.rds')
-exps=readRDS(file='./data/processed/GTEx/exps.rds')
+xx=list(br_up_GO,br_down_GO,gtex_up_GO,gtex_down_GO)
+signifGOs=unique(unlist(lapply(xx,function(x)unlist(lapply(x,function(a)a[a$p.adjusted<0.05,2])))))
+genelist=Reduce(union,list(br_up,br_down,gtex_up,gtex_down))
+gomat=matrix(NA,ncol=2,nrow=0)
 
-cormat=cormat[,grep('Brain',colnames(cormat),invert=T,v=T)]
+library(reshape2)
 
-library(biomaRt)
-humanmart=useMart('ensembl','hsapiens_gene_ensembl')
-getBM(attributes = c('ensembl_gene_id','hgnc_symbol','description'),filters = 'ensembl_gene_id',values = names(which(rowMeans(cormat>0)==1|rowMeans(cormat>0)==0)),mart = humanmart)
+for(x in xx){
+  for (a in x){
+    aa=a$genelist[a$Term%in%signifGOs]
+    names(aa)=a$Term[a$Term%in%signifGOs]
+    gomat=rbind(gomat,melt(aa))
+  }
+}
 
-cormat2=cormat
-cormat=cormat[complete.cases(cormat),]
-dim(cormat)
-# [1] 19456    22
+gomat=unique(gomat)
+library(igraph)
 
-quantile(rowSums(cormat>0),probs=c(0.005,1-0.01/2))
+gomat=graph_from_data_frame(gomat)
 
-xx=data.frame(`Datasets`=rowSums(cormat>0))
-xx$cons=c('-','Consistent')[1+(xx$Datasets>=20 | xx$Datasets <=3)]
-pdf('./results/GTEx/consistency_distribution_noBrain.pdf',width = 9)
-ggplot(xx,aes(x=Datasets,fill=cons))+
-  geom_bar(stat='count') +
-  theme_bw()+
-  theme(axis.title = element_text(size = 15),
-        axis.text = element_text(size=12),
-        plot.title = element_text(size=14)) +
-  scale_fill_manual(values=c('gray35','darkred'))+
-  xlab('Number of Datasets')+
-  ylab('Count')+
-  guides(fill=guide_legend(title='')) + 
-  ggtitle('For each gene, how many datasets have a positive correlation with age?\n*Irrespective of the effect size')
+library(GGally)
+
+cx=as.factor(1+grepl('ENSG',rownames(gomat[])))
+
+dx=rep(1,nrow(gomat[]))
+dx[rownames(gomat[])%in%union(br_up,br_down)]=2
+dx[rownames(gomat[])%in%union(gtex_up,gtex_down)]=3
+dx[rownames(gomat[])%in%intersect(union(br_up,br_down),union(gtex_up,gtex_down))]=4
+
+go_br_up=unname(unlist(sapply(br_up_GO,function(x)x$Term[x$p.adjusted<0.05])))
+go_br_down=unname(unlist(sapply(br_down_GO,function(x)x$Term[x$p.adjusted<0.05])))
+go_gtex_up=unname(unlist(sapply(gtex_up_GO,function(x)x$Term[x$p.adjusted<0.05])))
+go_gtex_down=unname(unlist(sapply(gtex_down_GO,function(x)x$Term[x$p.adjusted<0.05])))
+
+dx[rownames(gomat[])%in%union(go_br_up,go_br_down)]=2
+dx[rownames(gomat[])%in%union(go_gtex_up,go_gtex_down)]=3
+dx[rownames(gomat[])%in%intersect(union(go_br_up,go_br_down),union(go_gtex_down,go_gtex_up))]=3
+
+
+nmx=rownames(gomat[])
+nmx[grep('ENSG',nmx)]=''
+
+dx=factor(c('Brain Datasets','GTEx','Both')[dx-1],levels=c('Brain Datasets','GTEx','Both'))
+pdf('./results/GO-result.pdf',width = 35,height = 35)
+ggnet2(gomat,label = nmx,label.alpha = 1,label.color = 'gray35',
+       shape = cx,
+       color = dx,alpha = 0.75,edge.color = 'gray90',size=c(50,16)[cx],color.palette = 'Set1')+guides(color=guide_legend(title=''),shape=F,size=F)
 dev.off()
 
-ups=names(which(rowSums(cormat>0)>=20))
-downs=names(which(rowSums(cormat>0)<=3))
-
-ups2=as.character(read.table('./data/processed/humanBrainMicroarray_LuIncluded/ensembl_consistent_up.txt')[,1])
-downs2=as.character(read.table('./data/processed/humanBrainMicroarray_LuIncluded/ensembl_consistent_down.txt')[,1])
-
-xx=list(ups,downs,ups2,downs2)
-names(xx)=c('GTEx-up','GTEx-down','Brain_micro-up','Brain_micro-down')
-round(sapply(xx,function(x){
-  sapply(xx,function(y){
-    mean(x%in%y)
-  })
-}),3)*100
-
-upsp=unique(getBM(attributes = c('affy_hg_u133a'),filters = 'ensembl_gene_id',values = ups,mart = humanmart)[,1])
-downsp=unique(getBM(attributes = c('affy_hg_u133a'),filters = 'ensembl_gene_id',values = downs,mart = humanmart)[,1])
-
-upsp2=setdiff(upsp,downsp)
-downsp2=setdiff(downsp,upsp)
-
-write.table(upsp2,file = './data/processed/GTEx/nobrain_up.grp',quote = F,row.names = F,col.names = F)
-write.table(downsp2,file = './data/processed/GTEx/nobrain_down.grp',quote = F,row.names = F,col.names = F)
